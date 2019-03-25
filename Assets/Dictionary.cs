f59b3bc9f5efa9320beabed3ba1e3cb68a78a797
SergiuAvangarde <sergiu.pop@avangarde-software.com>
2019-03-25 17:53:03 +0200

    first comit
    tested dictionaries, and some UI elements

---
+++ Assets/Dictionary.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Dictionary : MonoBehaviour
+{
+    public SortedDictionary<string, string> words = new SortedDictionary<string, string>()
+    {
+        {"f", "definition for f"},
+        {"a", "definition for a"},
+        {"g", "definition for g"},
+        {"h", "definition for h"},
+        {"d", "definition for d"},
+        {"o", "definition for o"},
+        {"w", "definition for w"},
+        {"b", "definition for b"}
+    };
+
+    private void Start()
+    {
+        foreach(var word in words)
+        {
+            print("word: " + word.Key + " is: " + word.Value);
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+}
